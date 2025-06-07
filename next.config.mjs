d6b07831e2651697f4e291772652786d8c1c7d50
Mike <mike.beplus@gmail.com>
2025-06-07 10:25:29 +0700

    Update image source

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'previews.customer.envatousercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
